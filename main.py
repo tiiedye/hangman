9b3b6dbaaa10164e80647f74585cd525115d77d2
tiiedye <tiiedye@gmail.com>
2021-11-24 12:35:47 -0800

    writing simple steps to find if a guessed letter exists in a word randomly chosen from a list

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 # HANGMAN
+import random
 
+word_list = ["aardvark", "baboon", "camel"]
+
+# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = random.choice(word_list)
+
+# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input("What is your guess?: ")
+guess = guess.lower()
+
+# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+letter_found = False
+for ch in chosen_word:
+    if ch == guess:
+        letter_found = True
+
+if letter_found:
+    print("Correct!")
+else:
+    print("Wrong!")
