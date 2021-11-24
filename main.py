6eef8e757f7a6e97fdbec8179be5550d8171ea47
tiiedye <tiiedye@gmail.com>
2021-11-24 13:13:38 -0800

    adding the hangman pic, lives, and if you win or lose the game

--- main.py
+++ main.py
@@ -1,6 +1,63 @@
 # HANGMAN
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["aardvark", "baboon", "camel"]
 
 # Randomly choose a word from the word_list and assign it to a variable called chosen_word.
@@ -14,26 +71,27 @@ display = []
 for ch in chosen_word:
     display.append("_")
 
+lives = 6
 # While loop to allow the user to make multiple guesses
-while "_" in display:
+while "_" in display and lives > 0:
+    print(stages[lives])
     # Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
     guess = input("Guess a letter?: ").lower()
 
     # Loop through each position in the chosen_word, If the letter at that position matches 'guess' then reveal that
     # letter in the display at that position.
+    letter_found = False
     for i in range(len(chosen_word)):
         if chosen_word[i] == guess:
             display[i] = guess
-
-    # Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-    letter_found = False
-    for ch in chosen_word:
-        if ch == guess:
             letter_found = True
 
-    if letter_found:
-        print("Correct!")
-    else:
-        print("Wrong!")
+    if not letter_found:
+        lives -= 1
 
     print(display)
+
+if lives > 0:
+    print("You win!")
+else:
+    print("You lost!")
