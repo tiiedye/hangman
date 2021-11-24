eb26be4c93607311c6598423d2a0bf2d183a96b8
tiiedye <tiiedye@gmail.com>
2021-11-24 12:51:04 -0800

    creating a list with blanks that will fill in the correct guessed letters

--- main.py
+++ main.py
@@ -3,14 +3,27 @@ import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+# Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("What is your guess?: ")
-guess = guess.lower()
+# Testing code
+print(f'Pssst, the solution is {chosen_word}.')
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+# creates a list filled with "_" for every letting in chosen_word
+display = []
+for ch in chosen_word:
+    display.append("_")
+
+# Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input("Guess a letter?: ").lower()
+
+# Loop through each position in the chosen_word, If the letter at that position matches 'guess' then reveal that
+# letter in the display at that position.
+for i in range(len(chosen_word)):
+    if chosen_word[i] == guess:
+        display[i] = guess
+
+# Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 letter_found = False
 for ch in chosen_word:
     if ch == guess:
@@ -20,3 +33,5 @@ if letter_found:
     print("Correct!")
 else:
     print("Wrong!")
+
+print(display)
