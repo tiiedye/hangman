8f997377ee98705ed2179b7af2ca0a3fbf5621bc
tiiedye <tiiedye@gmail.com>
2021-11-24 12:57:43 -0800

    creating a while loop to allow the user to make multiple guesses

--- main.py
+++ main.py
@@ -14,24 +14,26 @@ display = []
 for ch in chosen_word:
     display.append("_")
 
-# Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter?: ").lower()
+# While loop to allow the user to make multiple guesses
+while "_" in display:
+    # Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+    guess = input("Guess a letter?: ").lower()
 
-# Loop through each position in the chosen_word, If the letter at that position matches 'guess' then reveal that
-# letter in the display at that position.
-for i in range(len(chosen_word)):
-    if chosen_word[i] == guess:
-        display[i] = guess
+    # Loop through each position in the chosen_word, If the letter at that position matches 'guess' then reveal that
+    # letter in the display at that position.
+    for i in range(len(chosen_word)):
+        if chosen_word[i] == guess:
+            display[i] = guess
 
-# Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-letter_found = False
-for ch in chosen_word:
-    if ch == guess:
-        letter_found = True
+    # Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+    letter_found = False
+    for ch in chosen_word:
+        if ch == guess:
+            letter_found = True
 
-if letter_found:
-    print("Correct!")
-else:
-    print("Wrong!")
+    if letter_found:
+        print("Correct!")
+    else:
+        print("Wrong!")
 
-print(display)
+    print(display)
